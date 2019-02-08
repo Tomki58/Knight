7ea87830ecb9f3f65f8841149317f1ae49acf506
Tomki58 <tomki589@gmail.com>
2019-02-08 12:33:42 +0300

    Initial commit

---
+++ Knight/main.cpp
@@ -0,0 +1,127 @@
+#include <utility>
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+// позиция на доске
+using pos = pair<char, int>; 
+
+// валидация позиции
+bool valid_position(pos next)
+{
+	return (next.first < 'a' || next.first > 'h'
+		|| next.second < 1 || next.second > 8) ? false : true;
+}
+
+// генератор позиций на поле
+string test_generator(char& lit, int& num)
+{
+	if (!valid_position(pos{ lit, num })) { num = 1; lit = 'a'; }
+	if (num == 9)
+	{
+		num = 1;
+		lit++;
+	}
+	return std::string(1, lit) + (char)(num++ + '0');
+}
+
+
+
+int knight(std::string start, std::string finish) 
+{
+	cout << start << '\t' << finish << '\n';
+	if (start == finish) return 0;
+
+	pos cur_pos{ start[0], atoi(&start[1]) },
+					fin_pos{finish[0], atoi(&finish[1])};
+
+	auto dif_x = fin_pos.first - cur_pos.first; // разница по горизонтали
+	auto dif_y = fin_pos.second - cur_pos.second; // разница по вертикали
+
+	int numberOfSteps{ 0 }; // количество шагов
+	
+
+	while (1)
+	{
+		int newDif_x, newDif_y;
+		int min_dif{ _MAX_INT_DIG };
+		vector<pos>* next_pos = new vector<pos>; // все возможные позиции
+		vector<pos>* golden_pos = new vector<pos>; // "золотые" позиции
+
+		// заполнение массива возможных позиций
+		if (valid_position({ cur_pos.first + 2, cur_pos.second + 1 }))
+			next_pos->push_back(pos{ cur_pos.first + 2, cur_pos.second + 1 });
+
+		if (valid_position({ cur_pos.first + 2, cur_pos.second - 1 }))
+			next_pos->push_back(pos{ cur_pos.first + 2, cur_pos.second - 1 });
+
+		if (valid_position({ cur_pos.first - 2, cur_pos.second + 1 }))
+			next_pos->push_back(pos{ cur_pos.first - 2, cur_pos.second + 1 });
+
+		if (valid_position({ cur_pos.first - 2, cur_pos.second - 1 }))
+			next_pos->push_back(pos{ cur_pos.first - 2, cur_pos.second - 1 });
+
+		if (valid_position({ cur_pos.first + 1, cur_pos.second + 2 }))
+			next_pos->push_back(pos{ cur_pos.first + 1, cur_pos.second + 2 });
+
+		if (valid_position({ cur_pos.first + 1, cur_pos.second - 2 }))
+			next_pos->push_back(pos{ cur_pos.first + 1, cur_pos.second - 2 });
+
+		if (valid_position({ cur_pos.first - 1, cur_pos.second + 2 }))
+			next_pos->push_back(pos{ cur_pos.first - 1, cur_pos.second + 2 });
+
+		if (valid_position({ cur_pos.first - 1, cur_pos.second - 2 }))
+			next_pos->push_back(pos{ cur_pos.first - 1, cur_pos.second - 2 });
+
+		// проход по каждой из возможных позиций с занесением в список "золотых"
+		for (auto& i : *next_pos)
+		{
+			if (i == fin_pos) return ++numberOfSteps;
+			newDif_x = fin_pos.first - i.first; newDif_y = fin_pos.second - i.second;
+			if (newDif_y != 0 && (abs((double)newDif_x / newDif_y) == 2 || abs((double)newDif_x / newDif_y) == 0.5))
+				golden_pos->emplace_back(i);
+		}
+
+		// проход по каждой из "золотых" позиций, чтобы сразу найти решение
+		if (!golden_pos->empty())
+			for (auto& i : *golden_pos)
+			{
+				min_dif = (abs(fin_pos.first - i.first) > abs(fin_pos.second - i.second)) ?
+					fin_pos.second - i.second : fin_pos.first - i.first;
+			}
+
+		if (min_dif != _MAX_INT_DIG)
+			return 1 + abs(min_dif) + numberOfSteps;
+
+		min_dif = abs(fin_pos.first - next_pos->at(0).first) + abs(fin_pos.second - next_pos->at(0).second); // сумма разниц координат
+		int min_idx = 0; // индекс позиции с минимальной суммой расстояний
+
+		// проход в сторону уменьшения суммы расстояний
+		for (auto i = next_pos->begin() + 1; i != next_pos->end(); i++)
+		{
+			if (abs(fin_pos.first - (*i).first) + abs(fin_pos.second + (*i).second) < min_dif)
+			{
+				min_dif = abs(fin_pos.first - (*i).first) + abs(fin_pos.second + (*i).second);
+				min_idx = i - next_pos->begin();
+			}
+			cur_pos = next_pos->at(min_idx);
+		}
+
+		numberOfSteps++; // окончание вычислений
+
+		delete next_pos;
+		delete golden_pos;
+	}
+
+	return 0;
+}
+
+int main()
+{
+	char lit{ 'a' }; int num{ 1 };
+	while (1)
+	{
+		std::cout << knight(test_generator(lit, num), "h8") << endl;
+	}
+}
